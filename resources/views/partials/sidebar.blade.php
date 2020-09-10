a8e04f97bf0df665646ad567e47ccbe1007d44ba
Jasper Frumau <jasper@imagewize.com>
2020-09-10 09:58:45 +0700

    load specific sidebars when need be

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,1 +1,10 @@
+@if (!is_page_template('template-sidebar-left.blade.php') || !is_page_template('template-sidebar-right.blade.php'))
 @php(dynamic_sidebar('sidebar-primary'))
+@endif
+@if (is_page_template('template-sidebar-left.blade.php'))
+@php(dynamic_sidebar('sidebar-left'))
+@endif
+@if (is_page_template('template-sidebar-right.blade.php'))
+@php(dynamic_sidebar('sidebar-right'))
+@endif
+
