4debd883c61d5de4079410706d892e39fbf174b2
Jasper Frumau <jasper@imagewize.com>
2020-09-13 08:58:36 +0700

    body class filter stub and comments cleanup

--- app/filters.php
+++ app/filters.php
@@ -16,14 +16,40 @@ add_filter('excerpt_more', function () {
 });
 
 /**
+ * Body Class Filter 
+ * 
+ * Allows adding of classes to body tag
+ * 
+ * WordPress offers filter hooks to allow plugins to modify various types of internal 
+ * data at runtime.
+ * @param mixed $classes
+ * 
+ */
+add_filter('body_class', __NAMESPACE__ . '\\body_class');
+
+function body_class($classes) {
+    
+    // Add class if it is sidebar left layout
+  if (is_page_template('template-sidebar-left.blade.php')) {
+
+      $classes[] = 'archive';
+
+  } else {
+    $classes[] = 'leading-normal tracking-normal text-white gradient';
+  }
+
+  return $classes;
+}
+
+/**
  * Pass data to page templates or all views
  * 
  * Sage includes a sage/template/{$class}/data filter that can be used to pass data to templates. 
- * This is the most simple way to pass data. The filter is based of body classes and can be used to target 
- * specific templates, for example: sage/template/home/data — Home page or sage/templae/page/data for all page templates
+ * The filter is based of body classes and can be used to target specific templates, for example: 
+ * sage/template/home/data — Home page or sage/templae/page/data for all page templates
  * 
- * NB View Composer can be used for this and any view in general as well. The $class can be used for loading from view composer and 
- * $classes['main'] to load main from Sage data action using share for all views. Both are possible. 
+ * View Composers can also be used for this and any view in general as well. The $class can be used for 
+ * loading from view composer and $classes['main'] to load main from Sage data action using share for all views.
  * Also see https://roots.io/working-with-composers-in-sage-10/ 
  * 
  * Example focussed on page templates:
@@ -38,13 +64,10 @@ add_filter('excerpt_more', function () {
 /**
  * Sections, Container, Main and Sidebar Classes 
  * 
- * Sage Share data in all views
+ * Tailwind utitly classes added to main classes in app layout. The Root Sage share 
+ * data function can be used in all views
  * 
- * Using view share to make the sharing cross views possible
- * Won't work currently with another variable preceding it
- * Used here for adding classes to main layout.
- * 
- * use {{ $classes['main'] }}
+ * example: use {{ $classes['main'] }} in view to load classes attached to main
  */
 add_action('the_post', function() { 
     // left sidebar with row reverse for sidebar positioning 
@@ -66,6 +89,7 @@ add_action('the_post', function() {
         'sidebar' => 'w-full md:w-1/4 p-6 flex flex-col flex-grow flex-shrink' 
         ]);
         }
+    /* more page conditionals to be added */
     // landing page or frontpage
     // blog index
     // blog single
